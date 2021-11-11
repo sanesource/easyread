db8ac7a414bdbfd3afbff9822652c1599ecf4989
Akul Srivastava <akulsr0@gmail.com>
2021-11-11 23:08:45 +0530

    feat: added loading bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import type { NextPage } from "next";
+import Image from "next/image";
 import Head from "next/head";
 import PDFObject from "pdfobject";
 
@@ -7,6 +8,7 @@ const Home: NextPage = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const viewerRef = useRef(null);
   const [currentBook, setCurrentBook] = useState<string>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   function getSecureResult(results: Array<string>) {
     results.forEach((result) => {
@@ -18,12 +20,18 @@ const Home: NextPage = () => {
   async function onClickSearch() {
     const input = inputRef.current!.value;
     if (currentBook !== input) {
-      const res = await fetch(`/api/results?input=${input}`);
-      const { results } = await res.json();
-      const url = getSecureResult(results);
-      if (url) {
-        setCurrentBook(input);
-        PDFObject.embed(url, viewerRef.current);
+      try {
+        setIsLoading(true);
+        const res = await fetch(`/api/results?input=${input}`);
+        const { results } = await res.json();
+        const url = getSecureResult(results);
+        if (url) {
+          setCurrentBook(input);
+          PDFObject.embed(url, viewerRef.current);
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
       }
     }
   }
@@ -46,6 +54,11 @@ const Home: NextPage = () => {
         <button onClick={onClickSearch}>Search</button>
       </header>
       <div id="viewer" ref={viewerRef}></div>
+      {isLoading && (
+        <div id="loading">
+          <Image src="/loading.gif" width={100} height={100} alt="loading" />
+        </div>
+      )}
     </div>
   );
 };
